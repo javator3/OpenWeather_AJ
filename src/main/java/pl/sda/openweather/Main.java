c139773f8aff9749519653a3891b9bd7a58f6897
kamisamapl <jagla1990@gmail.com>
2019-02-10 12:21:44 +0100

    3

--- src/main/java/pl/sda/openweather/Main.java
+++ src/main/java/pl/sda/openweather/Main.java
@@ -1,24 +1,23 @@
 package pl.sda.openweather;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import pl.sda.openweather.model.Weather;
-import java.io.IOException;
-import java.net.URL;
+import java.util.Scanner;
+
 
 public class Main {
-
     public static void main(String[] args) {
-
-        try {
-            URL jsonURL = new URL("https://api.apixu.com/v1/current.json?key=705dc215140747bbaba91901191002&q=Toru%C5%84");
-            ObjectMapper objectMapper = new ObjectMapper();
-            Weather weather =objectMapper.readValue(jsonURL, Weather.class);
-            System.out.println(weather);
-        } catch(IOException e) {
-            e.printStackTrace();
-
+        WeatherService weatherService = new WeatherService(
+                "http://api.apixu.com/v1/current.json",
+                "705dc215140747bbaba91901191002"
+        );
+        String city ;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Miasto: ");
+        while (!(city = scanner.nextLine()).equals("")) {
+            weatherService.getCityWeather(city);
+            System.out.println("Nastepne: ");
         }
+
 
     }
 }
